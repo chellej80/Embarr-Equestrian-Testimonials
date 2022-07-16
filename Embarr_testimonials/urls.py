8d63b3718317e4ac85581c83a0e7c47692c4cfc2
chellej80 <embarrequestrian@gmail.com>
2022-07-16 20:36:50 +0000

    add post list and post details to url patterns

--- Embarr_testimonials/urls.py
+++ Embarr_testimonials/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
