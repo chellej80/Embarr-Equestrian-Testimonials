1b8869b8a33e9d24a058c3dd2bededcd6f1f6bc6
chellej80 <embarrequestrian@gmail.com>
2022-07-24 20:33:06 +0000

    added bootstart shadow class to sign in container

--- Reviews/forms.py
+++ Reviews/forms.py
@@ -14,11 +14,11 @@ class UserUpdateForm(forms.ModelForm):
     Form for profile name update
     """
     email = forms.EmailField()
-    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'bio']
+        fields = ['username', 'email', 'first_name', 'last_name']
 
 
 class searchForm(forms.Form):
